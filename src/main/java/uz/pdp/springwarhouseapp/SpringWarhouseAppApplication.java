151f1da34a8debfb860e2f3fc8ced592fca3bc4f
DreamCode-Uz <javadeveloperbackend999@gmail.com>
2022-08-01 21:32:09 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/springwarhouseapp/SpringWarhouseAppApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springwarhouseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWarhouseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWarhouseAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/springwarhouseapp/SpringWarhouseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.springwarhouseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWarhouseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
