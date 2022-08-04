0b167484ec4efb34e24ac8030ba112d9907f1859
DreamCode-Uz <javadeveloperbackend999@gmail.com>
2022-08-04 14:16:04 +0500

    OutputProduct's crud operations have been written.

--- src/main/java/uz/pdp/springwarhouseapp/service/OutputProductService.java
+++ src/main/java/uz/pdp/springwarhouseapp/service/OutputProductService.java
@@ -31,12 +31,12 @@ public class OutputProductService {
     }
 
     //    GET ALL
-    public ResponseEntity<Page<OutputProduct>> getAll(Integer page, Integer size) {
+    public ResponseEntity<Page<OutputProduct>> getAllOutputProduct(Integer page, Integer size) {
         return new ResponseEntity<>(repository.findAll(PageRequest.of(page > 0 ? page - 1 : 0, size)), OK);
     }
 
     //    GET ONE
-    public ResponseEntity<?> getOne(Integer id) {
+    public ResponseEntity<?> getOneOutputProduct(Integer id) {
         Optional<OutputProduct> optionalOutputProduct = repository.findById(id);
         if (!optionalOutputProduct.isPresent()) return new ResponseEntity<>("Output product not found.", NOT_FOUND);
         return new ResponseEntity<>(optionalOutputProduct.get(), OK);
@@ -48,12 +48,27 @@ public class OutputProductService {
         if (!optionalProduct.isPresent()) return new ResponseEntity<>("Product not found.", NOT_FOUND);
         Optional<Output> optionalOutput = outputRepository.findById(dto.getOutputId());
         if (!optionalOutput.isPresent()) return new ResponseEntity<>("Output not found.", NOT_FOUND);
-        if (dto.getAmount() != null || dto.getAmount() == 0) return new ResponseEntity<>("Amount was not entered or the value remained 0", BAD_REQUEST);
-        if (dto.getPrice() != null || dto.getPrice() <= 0) return new ResponseEntity<>("Price entered incorrectly.", BAD_REQUEST);
+        if (dto.getAmount() != null || dto.getAmount() == 0)
+            return new ResponseEntity<>("Amount was not entered or the value remained 0", BAD_REQUEST);
+        if (dto.getPrice() != null || dto.getPrice() <= 0)
+            return new ResponseEntity<>("Price entered incorrectly.", BAD_REQUEST);
         OutputProduct op = new OutputProduct(optionalProduct.get(), dto.getAmount(), dto.getPrice(), optionalOutput.get());
         return new ResponseEntity<>(repository.save(op), CREATED);
     }
-//    UPDATE
+
+    //    UPDATE
+    public ResponseEntity<?> editOutputProduct(Integer id, OutputProductDTO dto) {
+        Optional<OutputProduct> oop = repository.findById(id);
+        if (!oop.isPresent()) return new ResponseEntity<>("Output product not found", NOT_FOUND);
+        OutputProduct outputProduct = oop.get();
+        Optional<Output> optionalOutput = outputRepository.findById(dto.getOutputId());
+        optionalOutput.ifPresent(outputProduct::setOutput);
+        Optional<Product> optionalProduct = productRepository.findById(dto.getProductId());
+        optionalProduct.ifPresent(outputProduct::setProduct);
+        if (dto.getPrice() != null && dto.getPrice() > 0) outputProduct.setPrice(dto.getPrice());
+        if (dto.getAmount() != null && dto.getAmount() > 0) outputProduct.setAmount(dto.getAmount());
+        return new ResponseEntity<>(repository.save(outputProduct),OK);
+    }
 
     //    DELETE
     public ResponseEntity<?> deleteOutput(Integer id) {
